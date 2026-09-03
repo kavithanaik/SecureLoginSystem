e93ae532c48ff7e1ccf536e807f7e3fc1b90756f
kavithanaik <kavitha.182005@gmail.com>
2026-09-03 12:08:17 +0530

    Update secure login system

--- app.py
+++ app.py
@@ -938,7 +938,7 @@ if __name__ == "__main__":
     print("")
 
     app.run(
-        host="127.0.0.1",
-        port=5000,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
         debug=True
     )
